3be67304cf9e23d818fa225b155794cc41cf69ed
Anna Kuzn <annbuho@gmail.com>
2021-01-19 13:55:43 +0300

    code style

--- src/components/articles/article/article.jsx
+++ src/components/articles/article/article.jsx
@@ -14,7 +14,8 @@ import noAvatarImg from '../../../img/noAvatar.jpg';
 import ChangeArticalButtons from '../changeArticalButtons/changeArticalButtons';
 
 
-const Article = ({ item, fullText, btns, user, favorites, changeFavoriteStatus}) => {
+const Article = ({ user, item, btns, favorites, fullText, changeFavoriteStatus }) => {
+	
 	const [ article, setArticle ] = useState(item);
 	const [ imageSrc, setImageSrc ] = useState(article.author.image);
 
@@ -28,14 +29,11 @@ const Article = ({ item, fullText, btns, user, favorites, changeFavoriteStatus})
 		return <li key={key} className={articleClasses.tag}>{tag}</li>;
 	});
 
-	const link = routePaths.article.replace(/:slug/, article.slug);
-
 	const buttons = btns ? <ChangeArticalButtons slug={article.slug} token={user.token} /> : null;
 
-	const body = fullText ? (
-		<pre className={articleClasses.fullText}>{fullText}</pre>
-	) : null;
-
+	const body = fullText ? <pre className={articleClasses.fullText}>{fullText}</pre> : null;
+	
+	const link = routePaths.article.replace(/:slug/, article.slug);
 
 	const like = favorites.includes(article.slug) ? redLikeImg : likeImg;
 	
@@ -48,29 +46,27 @@ const Article = ({ item, fullText, btns, user, favorites, changeFavoriteStatus})
 
 	return (
 		<div className={articleClasses.article}>
-				<Link to={link} className={articleClasses.title} >{article.title}</Link>
-				<img className={articleClasses.img} src={imageSrc} onError={imgDownloadErrorHandler} alt="avatar"/>
-				<span className={articleClasses.author} >{article.author.username}</span>
-				<span className={articleClasses.data}>{format(new Date(article.createdAt), 'PP')}</span>
-				<ul className={articleClasses.tags}>{tags}</ul>
-				<div className={articleClasses.favorites}>
-						<button type='submit' disabled={!user} className={articleClasses.favoritesBtn} onClick={favoritesBtnClickHandler}>
-							<img src={like} alt="like"/>
-						</button>
-						<span className={articleClasses.favoritesCount}>{article.favoritesCount}</span> 
-				</div>
-				{buttons}
-				
-				<p className={articleClasses.text}>{article.description}</p>
-                {body}
+			<Link to={link} className={articleClasses.title} >{article.title}</Link>
+			<img className={articleClasses.img} src={imageSrc} onError={imgDownloadErrorHandler} alt="avatar"/>
+			<span className={articleClasses.author} >{article.author.username}</span>
+			<span className={articleClasses.data}>{format(new Date(article.createdAt), 'PP')}</span>
+			<ul className={articleClasses.tags}>{tags}</ul>
+			<div className={articleClasses.favorites}>
+				<button type='submit' disabled={!user} className={articleClasses.favoritesBtn} onClick={favoritesBtnClickHandler}>
+					<img src={like} alt="like"/>
+				</button>
+				<span className={articleClasses.favoritesCount}>{article.favoritesCount}</span> 
+			</div>
+			{buttons}
+			<p className={articleClasses.text}>{article.description}</p>
+			{body}
 		</div>
 	);
 };
 
 const mapStateToProps = (state) => ({
-	loading: state.loading,
-	favorites: state.articles.favorites,
 	user: state.user.userData,
+	favorites: state.articles.favorites,
 });
 
 export default connect(mapStateToProps, actions)(Article);
@@ -78,17 +74,17 @@ export default connect(mapStateToProps, actions)(Article);
 Article.defaultProps = {
 	user: null,
 	item: null,
-	fullText: null,
 	btns: false,
 	favorites: [],
+	fullText: null,
 	changeFavoriteStatus: (() => {}),
 };
 
 Article.propTypes = {
 	user: PropTypes.instanceOf(Object),
-    item: PropTypes.instanceOf(Object),
-	fullText: PropTypes.string,
+	item: PropTypes.instanceOf(Object),
 	btns: PropTypes.bool,
 	favorites: PropTypes.arrayOf(PropTypes.string),
+	fullText: PropTypes.string,
 	changeFavoriteStatus: PropTypes.func,
 };
